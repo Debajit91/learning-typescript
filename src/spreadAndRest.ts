d3eb335481f312bfe8835f05317cd42a9aad24a8
Debajit91 <debajitroy544@gmail.com>
2025-11-12 14:53:15 +0600

    explore using and difference between spread and rest operator

--- src/spreadAndRest.ts
+++ src/spreadAndRest.ts
@@ -1,4 +1,5 @@
 // spread operator
+// use case = copying, merging arrays/objects, passing array elements as function arguments
 
 const friends = ['Utso', 'Shuvo'];
 
@@ -8,7 +9,7 @@ const collegeFriends = ['Shovon', 'Joy', 'Sumon'];
 
 friends.push(...schoolFriends);
 friends.push(...collegeFriends);
-console.log(friends);
+// console.log(friends);
 
 const user = {name:"Deb", contact: "01783388518"};
 
@@ -16,4 +17,13 @@ const otherInfo = {hobby: "coding", favoriteColor: "Blue"};
 
 const userInfo = {...user, ...otherInfo};
 
-console.log(userInfo);
+// console.log(userInfo);
+
+// rest operator
+// use case = function parameters (collects extra arguments), Destructuring (collects remaining properties)
+const sendInvite = (...friends: string[]) => {
+    friends.forEach((friend:string) => console.log(`Sent Invitation to ${friend}`))
+};
+sendInvite(...friends, 'Dev', 'Jeet');
+
+// Here ...friends in function parameter is Rest Operator and ...friends in function argument is Spread Operator
