a8c6f93673726c3eccb32daaddd5a94801c8cd68
Debajit91 <debajitroy544@gmail.com>
2025-11-12 16:41:17 +0600

    explore array destructuring

--- src/destructuring.ts
+++ src/destructuring.ts
@@ -1,22 +1,38 @@
 // object destructuring
 
-
 const user = {
-    id: 123,
-    name: {
-        firstName: "Deb",
-        middleName: "Jeet",
-        lastName: "Roy"
-    },
-    gender: "male",
-    age: 35,
-    favoriteColor: "blue"
+  id: 123,
+  name: {
+    firstName: "Deb",
+    middleName: "Jeet",
+    lastName: "Roy",
+  },
+  gender: "male",
+  age: 35,
+  favoriteColor: "blue",
 };
 
 // id = normal destructuring
 // name = nested destructuring
 // age: MyAge = name alias
-const {id, name:{firstName, lastName}, age: MyAge} = user;
-console.log("UserId:", id + ",", "Name:", firstName + ' ' + lastName + ",", 'My age is:', MyAge);
+const {
+  id,
+  name: { firstName, lastName },
+  age: MyAge,
+} = user;
+
+
+// console.log(
+//   "UserId:",
+//   id + ",",
+//   "Name:",
+//   firstName + " " + lastName + ",",
+//   "My age is:",
+//   MyAge
+// );
+
 
 // array destructuring
+const teamMembers = ['Utso', 'Bijoy', 'Nirob', 'Shuvo', 'Dev'];
+const [, , , teamCoLeader] = teamMembers; //skip element by using ','
+console.log(teamCoLeader);
