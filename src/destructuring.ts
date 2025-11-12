8e34946f68da680454a3d0235ab5a2c7013ff8b4
Debajit91 <debajitroy544@gmail.com>
2025-11-12 16:30:05 +0600

    object destructuing: normal, nested and name alias explored

---
+++ src/destructuring.ts
@@ -0,0 +1,22 @@
+// object destructuring
+
+
+const user = {
+    id: 123,
+    name: {
+        firstName: "Deb",
+        middleName: "Jeet",
+        lastName: "Roy"
+    },
+    gender: "male",
+    age: 35,
+    favoriteColor: "blue"
+};
+
+// id = normal destructuring
+// name = nested destructuring
+// age: MyAge = name alias
+const {id, name:{firstName, lastName}, age: MyAge} = user;
+console.log("UserId:", id + ",", "Name:", firstName + ' ' + lastName + ",", 'My age is:', MyAge);
+
+// array destructuring
